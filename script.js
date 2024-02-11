4bf0184efd5dbf1c765659e30472be3575fd9d8e
Glitch (glitch-hello-website) <none>
2024-02-11 02:05:26 +0000

    🏹⛩ Checkpoint ./script.js:64935206/1959 ./style.css:64935206/1066 ./index.html:64935206/4664

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function sendMessage() {
     const username = document.getElementById("username").value;
     const messageText = document.getElementById("message").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
 
-    if (username && messageText) {
+    if (username && messageText && date && time) {
         const messagesContainer = document.getElementById("messages");
 
         const messageDiv = document.createElement("div");
         messageDiv.classList.add("message");
 
         const messageContent = document.createElement("p");
-        messageContent.innerHTML = `<span>${username}:</span> ${messageText}`;
+        messageContent.innerHTML = `<span>${username}:</span> ${messageText}<br>Date: ${date} | Time: ${time}`;
 
         messageDiv.appendChild(messageContent);
         messagesContainer.appendChild(messageDiv);
@@ -17,7 +19,9 @@ function sendMessage() {
         // Clear the form fields after sending a message
         document.getElementById("username").value = "";
         document.getElementById("message").value = "";
+        document.getElementById("date").value = "";
+        document.getElementById("time").value = "";
     } else {
-        alert("Please enter both username and message.");
+        alert("Please fill in all fields.");
     }
 }
